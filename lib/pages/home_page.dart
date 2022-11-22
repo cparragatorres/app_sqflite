b1bdba511cc60c30484e94fc72563fda7ebfee4a
Chris-ASUS <humbertpt14@gmail.com>
2022-11-22 13:19:23 -0500

    For 2

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_codigo5_sqflite/db/db_admin.dart';
+import 'package:sqflite/sqflite.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,19 +10,54 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List books = [];
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    // DBAdmin.db.getBooksRaw();
-    // DBAdmin.db.getBooks();
+    getData();
+  }
 
-    DBAdmin.db.insertBook();
+  getData() {
+    DBAdmin.db.getBooks().then((value) {
+      books = value;
+      setState(() {});
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Books"),
+      ),
+      body: ListView.builder(
+        itemCount: books.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            title: Text(books[index]["title"]),
+          );
+        },
+      ),
+      // body: FutureBuilder(
+      //   future: DBAdmin.db.getBooksRaw(),
+      //   builder: (BuildContext context, AsyncSnapshot snap) {
+      //     if (snap.hasData) {
+      //       List bookList = snap.data;
+      //       return ListView.builder(
+      //         itemCount: bookList.length,
+      //         itemBuilder: (BuildContext context, int index){
+      //           return ListTile(
+      //             title: Text(bookList[index]["title"]),
+      //             subtitle: Text(bookList[index]["author"]),
+      //           );
+      //         },
+      //       );
+      //     }
+      //     return Text("ss");
+      //   },
+      // ),
+    );
   }
 }
