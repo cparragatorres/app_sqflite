18a7f2aa42e0038c96dbe5069732e943f7726064
Chris-ASUS <humbertpt14@gmail.com>
2022-11-22 14:00:56 -0500

    UI 2

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_codigo5_sqflite/db/db_admin.dart';
+import 'package:flutter_codigo5_sqflite/ui/utils/colors.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:sqflite/sqflite.dart';
 
 class HomePage extends StatefulWidget {
@@ -29,33 +31,116 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Books"),
+      backgroundColor: kPrimaryColor,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Good Morning",
+                            style: GoogleFonts.poppins(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w400,
+                              fontSize: 16.0,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 4.0,
+                          ),
+                          Text(
+                            "Carmen Elizabeth Juanita de Costabrava Cortez",
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: GoogleFonts.poppins(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 20.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(10.0),
+                      child: Image.network(
+                        "https://pbs.twimg.com/media/Dj4hjzuWwAEdRUB?format=jpg&name=900x900",
+                        height: 60,
+                        width: 60,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  width: 100,
+                  height: 3,
+                  decoration: BoxDecoration(
+                      color: kSecondaryColor,
+                      borderRadius: BorderRadius.circular(10.0)),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    hintText: "Search",
+                    hintStyle: GoogleFonts.poppins(
+                      fontSize: 14.0,
+                      color: kPrimaryColor.withOpacity(0.45),
+                    ),
+                    suffixIcon: Container(
+                      margin: const EdgeInsets.all(3.0),
+                      decoration: BoxDecoration(
+                        color: kSecondaryColor,
+                        borderRadius: BorderRadius.circular(12.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: kSecondaryColor.withOpacity(0.2),
+                            blurRadius: 10.0,
+                            offset: const Offset(4,4),
+                          ),
+                        ],
+                      ),
+                      child: const Icon(
+                        Icons.search,
+                        color: Colors.white,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                      borderRadius: BorderRadius.circular(14.0),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                      borderRadius: BorderRadius.circular(14.0),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
-      body: ListView.builder(
-        itemCount: books.length,
-        itemBuilder: (BuildContext context, int index) {
-          return ListTile(
-            title: Text(books[index]["title"]),
-          );
-        },
-      ),
-      // body: FutureBuilder(
-      //   future: DBAdmin.db.getBooksRaw(),
-      //   builder: (BuildContext context, AsyncSnapshot snap) {
-      //     if (snap.hasData) {
-      //       List bookList = snap.data;
-      //       return ListView.builder(
-      //         itemCount: bookList.length,
-      //         itemBuilder: (BuildContext context, int index){
-      //           return ListTile(
-      //             title: Text(bookList[index]["title"]),
-      //             subtitle: Text(bookList[index]["author"]),
-      //           );
-      //         },
-      //       );
-      //     }
-      //     return Text("ss");
+
+// //1RA FORMA DE MOSTRAR RAPIDAMENTE LOS ELEMENTOS DE LA BD
+
+      // body: ListView.builder(
+      //   itemCount: books.length,
+      //   itemBuilder: (BuildContext context, int index) {
+      //     return ListTile(
+      //       title: Text(books[index]["title"]),
+      //     );
       //   },
       // ),
     );
